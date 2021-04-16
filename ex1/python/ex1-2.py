ed0173871bcf83aaeeae235887aca0ece1650df5
Yusuke Nakanishi <yn4k4nishi.kit@gmail.com>
2021-04-16 23:12:25 +0900

    :sparkles: Feature : 課題1(2)終わり

--- ex1/python/ex1-2.py
+++ ex1/python/ex1-2.py
@@ -24,38 +24,63 @@ def dichotomy(max_x, min_x, error):
 
         if(num > 1000):
             print("error : time out")
-            return
+            return False
 
         num = num + 1
 
     return mid_x
 
 
-def main():
-    step = 0.001
-    min_x = 0
-    max_x = min_x + step
+# aとbの間にtan(x)の不連続点があるかチェックする
+def checkDiscontinuous(a, b):
+    # 第何象限か計算する
+    t_a = int(a / (np.pi / 2))
+    t_a = t_a % 4 + 1
 
-    while(max_x < 15):
-        if(f(min_x) == 0.0):
-            min_x = min_x + step
-            max_x = max_x + step
+    t_b = int(b / (np.pi / 2))
+    t_b = t_b % 4 + 1
+
+    # 第1象限と第2象限を跨ぐ場合はFalse
+    if(t_a == 1 and t_b == 2):
+        return False
+    elif(t_a == 2 and t_b == 1):
+        return False
+
+    # 第3象限と第4象限を跨ぐ場合はFalse
+    if(t_a == 3 and t_b == 4):
+        return False
+    elif(t_a == 4 and t_b == 3):
+        return False
+
+    return True
+
+
+def main():
+    step = 0.00001
+    min_x = -0.01
+    max_x = 15
+    now_x = min_x + step
+
+    print("step : {}".format(step))
+    print("range : {} ~ {}".format(min_x, max_x))
+    print()
+    print("|{:20s}|{:20s}|".format("   x", "   error"))
+    print("|" + "-" * 20 + "|" + "-" * 20 + "|")
+
+    while(now_x < max_x):
+        if(not checkDiscontinuous(min_x, now_x)):
+            min_x = now_x
+            now_x = min_x + step
             continue
 
-        if(f(min_x) * f(max_x) < 0):
-            print(min_x, max_x, f(min_x), f(max_x))
-            # min_x = dichotomy(f, max_x, min_x, 0.001)
-            print(dichotomy(max_x, min_x, 0.01))
-            min_x = max_x
-            max_x = min_x
-        max_x = max_x + step
+        if(f(min_x) * f(now_x) < 0):
+            ans_x = dichotomy(now_x, min_x, 0.00001)
+            print("|{:20.15f}|{:20.15f}|".format(ans_x, f(ans_x)))
+            min_x = now_x
+            now_x = min_x
+
+        now_x = now_x + step
 
 
 if __name__ == "__main__":
     main()
-    # min_x = 4.3
-    # max_x = 4.6
-
-    # print("f(min_x) = " + str(f(min_x)))
-    # print("f(max_x) = " + str(f(max_x)))
-    # print(dichotomy(f, 4.3, 4.6, 0.0001))
