7dc52566a3dc6da256312abed2340c59bb364aff
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-06-09 16:20:12 +0900

    :construction: WIP : 外部ファイルから読み込みできるように

--- ex2/c/ex2-2.c
+++ ex2/c/ex2-2.c
@@ -3,11 +3,17 @@
 #include "utils.h"
 
 static const int size = 4;
+// double complex M[4][4]={
+//     {  3+4j, -9j , 6-5j, 8+1j },
+//     { -4+5j,  7  , 2+3j, 4+1j },
+//     {  7-3j, 4-7j, -3j , 5-3j },
+//     {  1+8j, 9-6j, 1-1j,  -3  }
+// };
 double complex M[4][4]={
-    {  3+4j, -9j , 6-5j, 8+1j },
-    { -4+5j,  7  , 2+3j, 4+1j },
-    {  7-3j, 4-7j, -3j , 5-3j },
-    {  1+8j, 9-6j, 1-1j,  -3  }
+    { 0,0,0,0 },
+    { 0,0,0,0 },
+    { 0,0,0,0 },
+    { 0,0,0,0 }
 };
 
 void inverse(const int size, 
@@ -89,6 +95,8 @@ void dot(const int size,
 }
 
 int main(){
+
+    loadMatrix("matrix.csv", size, M);
 
     printf("M = \n");
     printMatrix(size, size, M);
