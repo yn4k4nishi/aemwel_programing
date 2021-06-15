b70978119a72c354b658827e2831a3e9cd63a08e
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-06-15 18:15:32 +0900

    :+1: Update : 分散曲線が途切れるのを修正

--- ex3/src/main.cpp
+++ ex3/src/main.cpp
@@ -1,16 +1,26 @@
+#include <cmath>
+#include <ios>
 #include <iostream>
+#include <iomanip>
 #include "dichotomy.h"
 
 int main(){
-    double dx = 15.0 / 500.0;
-    double dy = 15.0 / 50.0;
+    double dy_num = 1000.0;
 
-    double error = 1.0e-2;
+    double dx = 15.0 / 1000.0;
+    double dy = 15.0 / dy_num;
 
-    std::cout << "x , y" << std::endl;
+    double error = 1.0e-4;
+
+    std::cout << "x, y, vacumm, dielectric" << std::endl;
 
     for (double x = 0.0; x < 15; x += dx) {
-        for (double y = 0.0; y < 15; y += dy) {
+        std::cout << std::fixed << std::setprecision(15) << x << ",,";
+        std::cout << std::fixed << std::setprecision(15) << x << ",";
+        std::cout << std::fixed << std::setprecision(15) << x / std::sqrt(eps_r) << std::endl;
+
+        double dy = x / dy_num;
+        for (double y = 0.0; y < x; y += dy) {
             double e0, e1;
             if( (!f(x, y, &e0)) || (!f(x, y+dy, &e1)) ){
                 continue;
@@ -19,7 +29,9 @@ int main(){
             if( e0 * e1 < 0 ){
                 double ans;
                 if( dichotomy(x, y, y+dy, error, &ans) ){
-                    std::cout << x << " , " << ans << std::endl;
+                    // std::cout << x << " , " << ans << std::endl;
+                    std::cout << std::fixed << std::setprecision(15) << x << ",";
+                    std::cout << std::fixed << std::setprecision(15) << ans << std::endl;
                 }
             }
         }    
