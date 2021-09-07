85a1b6ddedb2c95ca5febdfdf4ddd6bd60f16af1
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-09-07 16:55:32 +0900

    :bug: Fix : yの探索領域を修正

--- ex4/src/main.c
+++ ex4/src/main.c
@@ -9,25 +9,26 @@ int main(){
     double x_min = -PI;
     double x_max = PI;
 
-    double y_min = 0.0;
-    double y_max = 10.0;
+    double y_min = 0.001;
+    double y_max = 20.0;
 
-    double x_step = (x_max - x_min) / 1000.0; // 位相の刻み幅
-    double y_step = (y_max - y_min) / 1000.0;
+    double x_step = (x_max - x_min) / 2000.0; // 位相の刻み幅
+    double y_step = (y_max - y_min) / 2000.0;
 
     printf("tolerance : %lf\n",tolerance);
     printf("x : %lf ~ %lf\n",x_min,x_max);
     printf("y : %lf ~ %lf\n",y_min,y_max);
     printf("\n");
 
-    printf("x, y, e\n");
+    printf("x, y\n");
 
-    for (double x = x_min; x < x_max ; x += x_step){
+    for (double x = x_min; x < x_max; x += x_step){
         
-        for (double y = y_min; y < y_max; y += y_step ){
-            double error, ans;
+        for (double y = y_min; y < y_max - y_step; y += y_step ){
+            double ans, t;
             if (dichotomy(x, y, y + y_step, tolerance, &ans)){
-                printf("%lf, %lf, %lf\n", x, ans, error);
+                f(x, ans, &t);
+                printf("%lf, %lf, %lf\n", x, ans, t);
             }
         }
         
