02563e4d68d3c247c4e0c42a1f5e038a4f82134d
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-09-07 15:43:38 +0900

    :+1: Update : 分散関係式の記述

---
+++ ex4/src/dichotomy.h
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+// 対象の関数
+// ゼロ除算などの計算できない場合はfalseを返す
+bool f(double x, double y, double *error){    
+    
+    // パラメータ
+    double L_R = 1.0;
+    double C_R = 1.0;
+    double L_L = 1.0;
+    double C_L = 1.0;
+
+    double w2_se = 1.0 / L_R / C_L;
+    double w2_sh = 1.0 / L_L / C_R;
+
+    *error = cos(x) - 1.0 + y*y * L_R *C_R / 2.0 * ( 1- w2_se /y /y ) * ( 1- w2_sh /y /y );
+    return true;
+}
+
+bool dichotomy(double x, double min_y, double max_y, double error, double *ans){
+    const int max_trials = 1e4;
+    
+    double t0, t1;
+    f(x, min_y, &t0);
+    f(x, max_y, &t1);
+
+    if( t0 * t1 > 0 ){
+        return false;
+    }
+
+    int num = 0;
+    double mid_y = (max_y + min_y) / 2.0;
+    f(x, mid_y, &t1);
+    while (abs(t1) > error) {
+        f(x, min_y, &t0);
+        f(x, mid_y, &t1);
+
+        if( t0 * t1 >= 0 ){
+            min_y = mid_y;
+        }else {
+            max_y = mid_y;
+        }
+
+        mid_y = (max_y + min_y) / 2.0;
+
+        if(num > max_trials){
+            return false;
+        }
+
+        num ++;
+    }
+
+    *ans = mid_y;
+    return true;
+}
