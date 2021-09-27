d460d2b109479fb30d83e82c0a22d7729deb67cf
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-09-27 12:57:47 +0900

    :sparkles: Feature : arc cosde

--- ex4/src/slide.py
+++ ex4/src/slide.py
@@ -1,13 +1,10 @@
 #! /bin/usr/python3
 
-from matplotlib import colors
 import numpy as np
-from scipy import optimize
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
-from tqdm import tqdm
 
-f = np.linspace(1, 10e9, 1e3)
+f = np.linspace(1, 10e9, 800)
 
 x_data = []
 y_data = []
@@ -30,29 +27,27 @@ d_v = 0.25 / (2.0 * np.pi * 1.5e9)  # 0.25rad @1.5GHz
 N = 5 # セル数
 
 def calc():
+    global x_data
+    global y_data
+
     x_data.clear()
     y_data.clear()
     S11_list.clear()
     S21_list.clear()
-    # 分散曲線
-    print("Calc Dispersion ...")
-    for beta_d in tqdm(np.linspace(-np.pi, np.pi, 1e3)):
-        def error(w):
-            theta = d_v * w 
-            Z = - 1j / w / C_0
-            Y = - 1j / w / L_0
-            return np.cos(beta_d) - (np.cos(theta) + Z*Y / 2.0 * np.cos(theta/2.0) * np.cos(theta/2.0) + 1j/2 * ( Z/Z_0 + Y/Y_0 ) * np.sin(theta)).real
 
-        w0 = w[0]
-        for w_ in w:
-            if error(w0).real * error(w_).real < 0:
-                x_data.append(beta_d)
-                y_data.append( 1/(2 * np.pi) * optimize.bisect(error, w0, w_) / 1e9)
-            w0 = w_
+    # 分散曲線
+    # print("Calc Dispersion ...")
+    theta = d_v * w 
+    Z = - 1j / w / C_0
+    Y = - 1j / w / L_0
+    cos_b_d = (np.cos(theta) + Z*Y / 2.0 * np.cos(theta/2.0) * np.cos(theta/2.0) + 1j/2 * ( Z/Z_0 + Y/Y_0 ) * np.sin(theta)).real
+    x_data = np.arccos(cos_b_d)
+    x_data = np.concatenate([x_data, -1*x_data]).tolist()
+    y_data = np.concatenate([f, f]).tolist()
 
     # S parameter
-    print("Calc S parameter ...")
-    for w_ in tqdm(w):
+    # print("Calc S parameter ...")
+    for w_ in w:
         theta = d_v * w_ 
         Z = - 1j / w_ / C_0
         Y = - 1j / w_ / L_0
@@ -76,13 +71,13 @@ def calc():
 
         S11_list.append(20*np.log10(abs(S11)))
         S21_list.append(20*np.log10(abs(S21)))
-    print("Finish Calc.\n")
+    # print("Finish Calc.\n")
 
 def main():
     calc()
 
     fig, (ax1, ax2) = plt.subplots(1,2,figsize=(15,8))
-    plt.subplots_adjust(left=0.1, bottom=0.35)
+    plt.subplots_adjust(left=0.1, bottom=0.25)
 
     l1 = ax1.scatter(x_data, y_data, s=1)
     l2, = ax2.plot(f/1e9, S11_list, label="S11")
@@ -98,15 +93,12 @@ def main():
     ax2.set_ylim(-30, 0)
     ax2.legend()
 
-    ax_Z0 = plt.axes([0.1, 0.20, 0.8, 0.03])
-    ax_L0 = plt.axes([0.1, 0.15, 0.8, 0.03])
-    ax_C0 = plt.axes([0.1, 0.10, 0.8, 0.03])
+    ax_Z0 = plt.axes([0.1, 0.15, 0.8, 0.03])
+    ax_L0 = plt.axes([0.1, 0.10, 0.8, 0.03])
+    ax_C0 = plt.axes([0.1, 0.05, 0.8, 0.03])
     slider_Z0 = Slider(ax_Z0, r"$Z_0$[$\Omega$]", 0, 200, valinit=50,  valstep=1)
     slider_L0 = Slider(ax_L0, r"$L_0$[nH]"      , 0, 50, valinit=13.2, valstep=0.01)
     slider_C0 = Slider(ax_C0, r"$C_0$[pF]"      , 0, 10, valinit= 0.5, valstep=0.01)
-
-    ax_button = plt.axes([0.8, 0.05, 0.1, 0.04])
-    button = Button(ax_button, "Calc", color="green",hovercolor='0.7')
 
     def update(val):
         global Z_0
@@ -118,7 +110,6 @@ def main():
         L_0 = slider_L0.val * 1e-9
         C_0 = slider_C0.val * 1e-12
 
-    def click(event):
         calc()
         data = []
         for i in range(len(x_data)):
@@ -131,7 +122,6 @@ def main():
     slider_Z0.on_changed(update)
     slider_L0.on_changed(update)
     slider_C0.on_changed(update)
-    button.on_clicked(click)
 
     plt.show()
 
