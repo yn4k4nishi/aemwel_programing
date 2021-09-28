56c21bb95b230cedc9dc65e102d73ec5d2196b4f
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-09-28 17:05:19 +0900

    :sparkles: Feature : スタブの再現

--- ex4/src/slide_stub.py
+++ ex4/src/slide_stub.py
@@ -12,19 +12,28 @@ y_data = []
 S11_list = []
 S21_list = []
 
-Z_0 = 50
+Z_0 = 50        # 直列の特性インピーダンス
 Y_0 = 1 / Z_0
 
-Z_P = 50 # ポートの入力インピーダンス
+Z_1 = 50        # 並列の特性インピーダンス
+Y_1 = 1 / Z_1
+
+Z_P = 50        # ポートの入力インピーダンス
 Y_P = 1/Z_P
 
 w = 2.0 * np.pi * f
 
+c     = 299792458   # 光速
+eps_r = 0.999991    # 誘電率
+mu_r  = 1           # 透磁率
+
+d_0 = 0.01  # 直列の伝送線路長さ
+d_1 = 0.01  # 並列の伝送線路長さ
+
 L_0 = 10e-9
 C_0 = 1.5e-12
-d_v = 0.25 / (2.0 * np.pi * 1.5e9)  # 0.25rad @1.5GHz
 
-N = 5 # セル数
+N = 5           # セル数
 
 def calc():
     global x_data
@@ -35,29 +44,23 @@ def calc():
     S11_list.clear()
     S21_list.clear()
 
-    # 分散曲線
-    # print("Calc Dispersion ...")
-    theta = d_v * w 
-    Z = - 1j / w / C_0
-    Y = - 1j / w / L_0
-    cos_b_d = (np.cos(theta) + Z*Y / 2.0 * np.cos(theta/2.0) * np.cos(theta/2.0) + 1j/2 * ( Z/Z_0 + Y/Y_0 ) * np.sin(theta)).real
-    x_data = np.arccos(cos_b_d)
-    x_data = np.concatenate([x_data, -1*x_data]).tolist()
-    y_data = np.concatenate([f, f]).tolist()
-
-    # S parameter
-    # print("Calc S parameter ...")
     for w_ in w:
-        theta = d_v * w_ 
+        k = w_ / c * np.sqrt(eps_r * mu_r)
+        theta_0 = k * d_0
+        theta_1 = k * d_1 
         Z = - 1j / w_ / C_0
-        Y = - 1j / w_ / L_0
+        # Y = - 1j / w_ / L_0
+        Y = 1 / (1j * Z_1 * np.tan(theta_1))
 
         F_unit = np.array([[1, Z/2],[0,1]])
-        F_unit = np.dot(F_unit, np.array([[np.cos(theta/2), 1j*Z_0*np.sin(theta/2)],[1j*Y_0*np.sin(theta/2), np.cos(theta/2)]]))
+        F_unit = np.dot(F_unit, np.array([[np.cos(theta_0/2), 1j*Z_0*np.sin(theta_0/2)],[1j*Y_0*np.sin(theta_0/2), np.cos(theta_0/2)]]))
         F_unit = np.dot(F_unit, np.array([[1,0],[Y,1]]))
-        F_unit = np.dot(F_unit, np.array([[np.cos(theta/2), 1j*Z_0*np.sin(theta/2)],[1j*Y_0*np.sin(theta/2), np.cos(theta/2)]]))
+        F_unit = np.dot(F_unit, np.array([[np.cos(theta_0/2), 1j*Z_0*np.sin(theta_0/2)],[1j*Y_0*np.sin(theta_0/2), np.cos(theta_0/2)]]))
         F_unit = np.dot(F_unit, np.array([[1, Z/2],[0,1]]))
         
+        # 分散曲線
+        x_data.append(np.arccos( 1/2*(F_unit[0][0] + F_unit[1][1]) ))
+
         F = F_unit
         for _ in range(N-1):
             F = np.dot(F,F_unit)
@@ -67,18 +70,21 @@ def calc():
         C = F[1][0]
         D = F[1][1]
 
+        # S parameter
         S11 = 1/(A + B/Z_P + C*Z_P + D) * (A + B/Z_P - C*Z_P - D)
         S21 = 1/(A + B/Z_P + C*Z_P + D) * 2
 
         S11_list.append(20*np.log10(abs(S11)))
         S21_list.append(20*np.log10(abs(S21)))
-    # print("Finish Calc.\n")
+
+    x_data[len(x_data):len(x_data)] = [-i for i in x_data]
+    y_data = np.concatenate([f, f]).tolist()
 
 def main():
     calc()
 
     fig, (ax1, ax2) = plt.subplots(1,2,figsize=(15,8))
-    plt.subplots_adjust(left=0.1, bottom=0.25)
+    plt.subplots_adjust(left=0.1, bottom=0.30)
 
     l1 = ax1.scatter(x_data, y_data, s=1)
     l2, = ax2.plot(f/1e9, S11_list, label="S11")
@@ -94,22 +100,32 @@ def main():
     ax2.set_ylim(-30, 3)
     ax2.legend()
 
-    ax_Z0 = plt.axes([0.1, 0.15, 0.8, 0.03])
-    ax_L0 = plt.axes([0.1, 0.10, 0.8, 0.03])
-    ax_C0 = plt.axes([0.1, 0.05, 0.8, 0.03])
-    slider_Z0 = Slider(ax_Z0, r"$Z_0$[$\Omega$]", 0, 200, valinit= 50 , valstep= 1)
-    slider_L0 = Slider(ax_L0, r"$L_0$[nH]"      , 0, 50 , valinit= 10 , valstep= 0.01)
-    slider_C0 = Slider(ax_C0, r"$C_0$[pF]"      , 0, 10 , valinit= 1.5, valstep= 0.01)
+    ax_4 = plt.axes([0.1, 0.20, 0.8, 0.03])
+    ax_3 = plt.axes([0.1, 0.15, 0.8, 0.03])
+    ax_2 = plt.axes([0.1, 0.10, 0.8, 0.03])
+    ax_1 = plt.axes([0.1, 0.05, 0.8, 0.03])
+
+    slider_Z1 = Slider(ax_4, r"$Z_1$[$\Omega$]", 0, 200, valinit= 50 , valstep= 1)
+    slider_Z0 = Slider(ax_3, r"$Z_0$[$\Omega$]", 0, 200, valinit= 50 , valstep= 1)
+    slider_d1 = Slider(ax_2, r"$d_1$[mm]"      , 0, 15 , valinit= 5 ,  valstep= 0.1)
+    slider_d0 = Slider(ax_1, r"$d_0$[mm]"      , 0, 15 , valinit= 5 ,  valstep= 0.1)
 
     def update(val):
+        global Z_1
         global Z_0
+        global Y_1
         global Y_0
-        global L_0
-        global C_0
+        global d_1
+        global d_0
+
         Z_0 = slider_Z0.val
         Y_0 = 1/Z_0
-        L_0 = slider_L0.val * 1e-9
-        C_0 = slider_C0.val * 1e-12
+
+        Z_1 = slider_Z1.val
+        Y_1 = 1/Z_1
+
+        d_0 = slider_d0.val / 1000
+        d_1 = slider_d1.val / 1000
 
         calc()
         data = []
@@ -120,9 +136,10 @@ def main():
         l3.set_ydata(S21_list)
         fig.canvas.draw_idle()
 
+    slider_Z1.on_changed(update)
     slider_Z0.on_changed(update)
-    slider_L0.on_changed(update)
-    slider_C0.on_changed(update)
+    slider_d1.on_changed(update)
+    slider_d0.on_changed(update)
 
     plt.show()
 
